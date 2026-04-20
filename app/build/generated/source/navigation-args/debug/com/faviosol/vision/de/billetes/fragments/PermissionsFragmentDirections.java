910d2cf101642042c3f1d7be61e1d7b310e78f45
faviosol <fsolorzanov@ulasalle.edu.pe>
2026-04-19 23:43:07 -0500

    feat: refactor package name to com.faviosol and fix navigation
    - Se cambió el paquete base a com.faviosol.vision.de.billetes.
    - Se corrigieron imports de Detection y OverlayView.
    - Se arregló el nav_graph.xml para generar CameraFragmentDirections.
    - Build exitoso en Xiaomi.

---
+++ app/build/generated/source/navigation-args/debug/com/faviosol/vision/de/billetes/fragments/PermissionsFragmentDirections.java
@@ -0,0 +1,16 @@
+package com.faviosol.vision.de.billetes.fragments;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.ActionOnlyNavDirections;
+import androidx.navigation.NavDirections;
+import com.faviosol.vision.de.billetes.R;
+
+public class PermissionsFragmentDirections {
+  private PermissionsFragmentDirections() {
+  }
+
+  @NonNull
+  public static NavDirections actionPermissionsToCamera() {
+    return new ActionOnlyNavDirections(R.id.action_permissions_to_camera);
+  }
+}
